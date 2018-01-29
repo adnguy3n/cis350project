ccd6ddf7dc7b2e0a2fabdef1d6b862d26835b173
Joshua Goddard <goddardj@mail.gvsu.edu>
2018-01-29 10:35:05 -0500

    Test Commit

--- cis350project/src/testPackage/TestClass.java
+++ cis350project/src/testPackage/TestClass.java
@@ -2,9 +2,12 @@ package testPackage;
 
 public class TestClass {
 
+
+
 	public static void main(String[] args) {
 		// Something Something Comment
-		System.out.print("Anthony was here.");
+		System.out.println("Anthony was here.");
+		System.out.println("Josh was too.");
 	}
 
 }
