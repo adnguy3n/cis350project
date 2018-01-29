b1e791171dd82e62a5618666dbd600b125dcff6b
Luke <Luke@35.40.125.121>
2018-01-29 10:34:45 -0500

    test

--- cis350project/src/testPackage/TestClass.java
+++ cis350project/src/testPackage/TestClass.java
@@ -5,6 +5,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		// Something Something Comment
 		System.out.print("Anthony was here.");
+		System.out.print("Luke was also here.");
+		
 	}
 
 }
