7ad376dca41b4c74152a109c8c3e56e500a7292d
adnguy3n <nguyeant@mail.gvsu.edu>
2018-04-23 21:48:12 -0400

    Fixed merge error and added more JUnit tests.

--- cis350project/src/casinogames/CasinoGamesPokerTest.java
+++ cis350project/src/casinogames/CasinoGamesPokerTest.java
@@ -86,6 +86,26 @@ public class CasinoGamesPokerTest {
 	}
 	
 	/**
+	 * Tests for three of a kind.
+	 */
+	@Test
+	public void testIsThreeOfAKind01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.ACE, Suit.HEARTS);
+		Card card2 = new Card(CardValue.ACE, Suit.CLUBS);
+		Card card3 = new Card(CardValue.KING, Suit.DIAMONDS);
+		Card card4 = new Card(CardValue.TWO, Suit.HEARTS);
+		Card card5 = new Card(CardValue.THREE, Suit.CLUBS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false, (PokerResult.THREE_OF_A_KIND == game.getResult()));
+
+	}
+	
+	/**
 	 * Tests for Royal Pair.
 	 */
 	@Test
@@ -105,11 +125,28 @@ public class CasinoGamesPokerTest {
 		
 	}
 	
+	@Test
+	public void testisRoyalPair01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.ACE, Suit.HEARTS);
+		Card card2 = new Card(CardValue.KING, Suit.CLUBS);
+		Card card3 = new Card(CardValue.FOUR, Suit.DIAMONDS);
+		Card card4 = new Card(CardValue.TWO, Suit.HEARTS);
+		Card card5 = new Card(CardValue.THREE, Suit.CLUBS);
+		game.getPlayer().addToHand(card1);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card5);
+		assertEquals(false, (PokerResult.ROYAL_PAIR == game.getResult()));
+		
+	}
+	
 	/**
 	 * Tests for four of a kind.
 	 */
 	@Test
-	public void testIsFourOfAKind() {
+	public void testIsFourOfAKind00() {
 		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
 		Card card1 = new Card(CardValue.TWO, Suit.HEARTS);
 		Card card2 = new Card(CardValue.TWO, Suit.CLUBS);
@@ -123,6 +160,22 @@ public class CasinoGamesPokerTest {
 		game.getPlayer().addToHand(card1);
 		assertEquals(PokerResult.FOUR_OF_A_KIND, game.getResult());
 
+	}
+	
+	@Test
+	public void testIsFourOfAKind01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.TWO, Suit.HEARTS);
+		Card card2 = new Card(CardValue.TWO, Suit.CLUBS);
+		Card card3 = new Card(CardValue.TWO, Suit.DIAMONDS);
+		Card card4 = new Card(CardValue.SEVEN, Suit.SPADES);
+		Card card5 = new Card(CardValue.THREE, Suit.CLUBS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false, (PokerResult.FOUR_OF_A_KIND == game.getResult()));
 	}
 	
 	/**
@@ -142,6 +195,22 @@ public class CasinoGamesPokerTest {
 		game.getPlayer().addToHand(card2);
 		game.getPlayer().addToHand(card1);
 		assertEquals(PokerResult.TWO_PAIR, game.getResult());
+	}
+	
+	@Test
+	public void testIsTwoPair01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.TWO, Suit.HEARTS);
+		Card card2 = new Card(CardValue.TEN, Suit.CLUBS);
+		Card card3 = new Card(CardValue.TEN, Suit.DIAMONDS);
+		Card card4 = new Card(CardValue.TEN, Suit.SPADES);
+		Card card5 = new Card(CardValue.TEN, Suit.CLUBS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false, (PokerResult.TWO_PAIR == game.getResult()));
 	}
 	
 	/**
@@ -164,6 +233,22 @@ public class CasinoGamesPokerTest {
 		
 	}
 	
+	@Test
+	public void testIsFlush01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.ACE, Suit.HEARTS);
+		Card card2 = new Card(CardValue.SEVEN, Suit.HEARTS);
+		Card card3 = new Card(CardValue.EIGHT, Suit.HEARTS);
+		Card card4 = new Card(CardValue.FOUR, Suit.CLUBS);
+		Card card5 = new Card(CardValue.TWO, Suit.HEARTS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false,(PokerResult.FLUSH == game.getResult()));
+	}
+	
 	/**
 	 * Test for Royal Flush.
 	 */
@@ -181,6 +266,23 @@ public class CasinoGamesPokerTest {
 		game.getPlayer().addToHand(card2);
 		game.getPlayer().addToHand(card1);
 		assertEquals(PokerResult.ROYAL_FLUSH, game.getResult());
+		
+	}
+	
+	@Test
+	public void testIsRoyalFlush01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.ACE, Suit.SPADES);
+		Card card2 = new Card(CardValue.KING, Suit.HEARTS);
+		Card card3 = new Card(CardValue.QUEEN, Suit.HEARTS);
+		Card card4 = new Card(CardValue.TEN, Suit.HEARTS);
+		Card card5 = new Card(CardValue.JACK, Suit.HEARTS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false, (PokerResult.ROYAL_FLUSH == game.getResult()));
 		
 	}
 	
@@ -203,6 +305,21 @@ public class CasinoGamesPokerTest {
 		assertEquals(PokerResult.STRAIGHT_FLUSH, game.getResult());
 	}
 	
+	@Test
+	public void testIsStraightFlush01() {
+		CasinoGamesPokerModel game = new CasinoGamesPokerModel(1);
+		Card card1 = new Card(CardValue.SIX, Suit.HEARTS);
+		Card card2 = new Card(CardValue.SEVEN, Suit.SPADES);
+		Card card3 = new Card(CardValue.EIGHT, Suit.HEARTS);
+		Card card4 = new Card(CardValue.NINE, Suit.HEARTS);
+		Card card5 = new Card(CardValue.TEN, Suit.HEARTS);
+		game.getPlayer().addToHand(card5);
+		game.getPlayer().addToHand(card4);
+		game.getPlayer().addToHand(card3);
+		game.getPlayer().addToHand(card2);
+		game.getPlayer().addToHand(card1);
+		assertEquals(false, (PokerResult.STRAIGHT_FLUSH == game.getResult()));
+	}
 
 
 }
--- cis350project/src/casinogames/Player.java
+++ cis350project/src/casinogames/Player.java
@@ -27,11 +27,6 @@ public class Player {
 		hand.add(card);
 	}
 	
-	/**get held cards method */
-	public ArrayList<Boolean> getHolding(){
-		return holding;
-	}
-	
 	/**
 	 * Add Card to hand at index.
 	 * 
